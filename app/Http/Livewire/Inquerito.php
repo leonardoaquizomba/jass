c6fa98322b891712365b19bc760e3aaae895cda2
Leonardo Quizomba <leonardoaquizomba@hotmail.com>
2022-04-12 18:06:15 +0100

    new componets

---
+++ app/Http/Livewire/Inquerito.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Inquerito extends Component
+{
+    public function render()
+    {
+        return view('livewire.inquerito');
+    }
+}
