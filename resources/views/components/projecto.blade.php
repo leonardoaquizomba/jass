13836852b4a267ffc004386add02ef1655c6d7e7
Leonardo Quizomba <leonardoaquizomba@hotmail.com>
2022-04-19 14:17:04 +0100

    Update Projecto

--- resources/views/components/projecto.blade.php
+++ resources/views/components/projecto.blade.php
@@ -1,85 +1,34 @@
 <div class="section section-projects recent-project section-pad">
     <div class="container">
         <div class="content row">
-
             <h2 class="heading-section center">Projetos</h2>
-            <!-- Projects/Feature Row  -->
             <div class="feature-row feature-project-row mgfix">
                 <div class="owl-carousel loop has-carousel" data-items="4" data-navs="true">
+                    @forelse (App\Models\Projecto::all() as $projecto)
                     <div class="even">
-                        <!-- featured box  -->
                         <div class="feature feature-project boxed">
-                            <a href="project-single.html">
+                            <a href="{{ route('site.projecto_info', ['id'=>$projecto->id]) }}">
                                 <div class="fbox-photo">
-                                    <img src="image/work-sm-a.jpg" alt="">
+                                    <img src="@if ($projecto->imagens == null)
+                                    {{ asset('image/gallery/gallery1.jpg') }}
+                                    @else
+                                        {{ asset(Voyager::image(json_decode($projecto->imagens)[0])) }}
+                                @endif" alt="">
                                 </div>
                             </a>
                             <div class="fbox-content">
-                                <h3><a href="project-single.html">Altria Warehouse Complex</a></h3>
+                                <h3>
+                                    <a href="{{ route('site.projecto_info', ['id'=>$projecto->id]) }}">{{
+                                        $projecto->titulo }}</a>
+                                </h3>
                             </div>
                         </div>
-                        <!-- End -->
                     </div>
-                    <div class="odd">
-                        <!-- featured box -->
-                        <div class="feature feature-project boxed">
-                            <a href="project-single.html">
-                                <div class="fbox-photo">
-                                    <img src="image/work-sm-c.jpg" alt="">
-                                </div>
-                            </a>
-                            <div class="fbox-content">
-                                <h3><a href="project-single.html">Apollo Hill Project</a></h3>
-                            </div>
-                        </div>
-                        <!-- End -->
-                    </div>
-                    <div class="even">
-                        <!-- featured box -->
-                        <div class="feature feature-project boxed">
-                            <a href="project-single.html">
-                                <div class="fbox-photo">
-                                    <img src="image/work-sm-b.jpg" alt="">
-                                </div>
-                            </a>
-                            <div class="fbox-content">
-                                <h3><a href="project-single.html">Balko Wind Power</a></h3>
-                            </div>
-                        </div>
-                        <!-- End -->
-                    </div>
-                    <div class="odd">
-                        <!-- featured box -->
-                        <div class="feature feature-project boxed">
-                            <a href="project-single.html">
-                                <div class="fbox-photo">
-                                    <img src="image/work-sm-e.jpg" alt="">
-                                </div>
-                            </a>
-                            <div class="fbox-content">
-                                <h3><a href="project-single.html">Rocky Forge Refinary</a></h3>
-                            </div>
-                        </div>
-                        <!-- End -->
-                    </div>
-                    <div class="odd">
-                        <!-- featured box -->
-                        <div class="feature feature-project boxed">
-                            <a href="project-single.html">
-                                <div class="fbox-photo">
-                                    <img src="image/work-sm-f.jpg" alt="">
-                                </div>
-                            </a>
-                            <div class="fbox-content">
-                                <h3><a href="project-single.html">Mingo Village Mining</a></h3>
-                            </div>
-                        </div>
-                        <!-- End -->
-                    </div>
+                    @empty
+
+                    @endforelse
                 </div>
             </div>
-            <!-- Projects/Feature Row  #end -->
-
         </div>
     </div>
 </div>
